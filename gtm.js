7f54c7f084de5049ce95e0099142bdbaf2f6906e
Hasan Prayoga <hasandotprayoga@gmail.com>
2023-11-21 13:20:47 +0700

    Update gtm.js

--- gtm.js
+++ gtm.js
@@ -1,43 +1,38 @@
- // Function to make HTTP request
-        function fetchData() {
-           const urlParams = new URLSearchParams(window.location.search);
+// Function to make HTTP request
+function fetchData() {
+    const urlParams = new URLSearchParams(window.location.search);
 
-// Get the value of a specific parameter
-const id = urlParams.get('id');
+    // Get the value of the 'id' parameter
+    const id = urlParams.get('id');
 
-         console.log(id)
-         
-            fetch('https://gateway.yukbisnis.dev/v1/analytic?status=ACTIVE&type=GOOGLE_ANALYTIC&idProduct=' + id)
-                .then(response => response.json())
-                .then(data => {
-                    if (data.response.results.items.length > 0) {
-                        // Extract Google Analytics ID from the response
-                        const googleAnalyticsId = data.response.results.items[0].AnalyticValue;
+    if (id) {
+        console.log('ID:', id);
 
-                        // Call function to set up Google Tag Manager with the retrieved ID
-                        setupGoogleTagManager(googleAnalyticsId);
+        // Make the HTTP request using the id parameter
+        fetch('https://gateway.yukbisnis.dev/v1/analytic?status=ACTIVE&type=GOOGLE_ANALYTIC&idProduct=' + id)
+            .then(response => response.json())
+            .then(data => {
+                if (data.response.results.items.length > 0) {
+                    // Extract Google Analytics ID from the response
+                    const googleAnalyticsId = data.response.results.items[0].AnalyticValue;
 
-                    }
-                })
-                .catch(error => console.error('Error fetching data:', error));
-        }
+                    // Call function to set up Google Tag Manager with the retrieved ID
+                    setupGoogleTagManager(googleAnalyticsId);
+                }
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    } else {
+        console.error('ID parameter not found in the URL.');
+    }
+}
 
-        // Function to set up Google Tag Manager with the Google Analytics ID
-        function setupGoogleTagManager(googleAnalyticsId) {
-            // Google Tag Manager container code
-            (function (w, d, s, l, i) {
+// Function to set up Google Tag Manager with the Google Analytics ID
+function setupGoogleTagManager(googleAnalyticsId) {
+    // Google Tag Manager container code
+    (function (w, d, s, l, i) {
+        var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true; j.src = 'https://www.googletagmanager.com/gtag/js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', googleAnalyticsId);
 
-                var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-                j.async = true; j.src = 'https://www.googletagmanager.com/gtag/js?id=' + i + dl;
-                f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', googleAnalyticsId);
-
-
-            window.dataLayer = window.dataLayer || [];
-            function gtag() { dataLayer.push(arguments); }
-            gtag('js', new Date());
-
-            gtag('config', googleAnalyticsId);
-        }
-
-        fetchData()
+    window.dataLayer = window.da
